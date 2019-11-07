95f95f3f8c51836081f1197238cd6d7a9139bfba
lisperow <lisperow@yandex.ru>
2019-11-07 14:10:55 +0300

    add methods in Field

--- src/xo/Main.java
+++ src/xo/Main.java
@@ -1,7 +1,9 @@
 package xo;
 
 public class Main {
-    public static void main(String... args) {
-        System.out.println("Hello, Ivan");
+
+    public static void main(String[] args) {
+        System.out.println("Hi!")
     }
+
 }
--- src/xo/model/Field.java
+++ src/xo/model/Field.java
@@ -1,3 +1,19 @@
 package xo.model;
 
-public class Field {}
+public class Field {
+
+    private String[][] figures = new String[3][3];
+
+    public int getSize() {
+        return figures.length;
+    }
+
+    public String getFigure(final Point point) {
+        return figures[point.x][point.y];
+    }
+
+    public void setFigure(final Point point, final String figure) {
+        figures[point.x][point.y] = figure;
+    }
+
+}
